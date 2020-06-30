dbf1be2b3235f49e8338b7329160b13e849b8bb6
jianglong <jianglong@sune365.com.cn>
2020-06-30 15:52:26 +0800

    [add]添加注释，便于理解

--- src/pages/spec-choose.tsx
+++ src/pages/spec-choose.tsx
@@ -1,56 +1,66 @@
-import React, { useState, useMemo } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/reducer/root-reducer";
+import React, {useState, useMemo} from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "../redux/reducer/root-reducer";
 import SpecAdjoinMatrix from "../utils/spec-adjoin-martix";
 import "./spec.css";
+import {is} from "@babel/types";
+
 const classNames = require("classnames");
 
 const Spec: React.FC = () => {
-  const { specList, specCombinationList } = useSelector((state: RootState) => state.spec);
-  // 已选择的规格，长度为规格列表的长度
-  const [specsS, setSpecsS] = useState(Array(specList.length).fill(""));
+    const {specList, specCombinationList} = useSelector((state: RootState) => state.spec);
+    // 已选择的规格，长度为规格列表的长度
+    //useState  相当于在state中申明了一下specsS中变量
+    const [specsS, setSpecsS] = useState(Array(specList.length).fill(""));
 
-  // 创建一个规格矩阵
-  const specAdjoinMatrix = useMemo(() => new SpecAdjoinMatrix(specList, specCombinationList), [specList, specCombinationList]);
-  // 获得可选项表
-  const optionSpecs = specAdjoinMatrix.getSpecscOptions(specsS);
+    // 创建一个规格矩阵
+    //useMemo是在变量发生变化时，会再次执行
+    const specAdjoinMatrix = useMemo(() => new SpecAdjoinMatrix(specList, specCombinationList), [specList, specCombinationList]);
+    // 获得可选项表
+    const optionSpecs = specAdjoinMatrix.getSpecscOptions(specsS);
 
-  const handleClick = function (bool: boolean, text: string, index: number) {
-    // 排除可选规格里面没有的规格
-    if (specsS[index] !== text && !bool) return;
-    // 根据text判断是否已经被选中了
-    specsS[index] = specsS[index] === text ? "" : text;
-    setSpecsS(specsS.slice());
-  };
+    const handleClick = function (bool: boolean, text: string, index: number) {
+        // 排除可选规格里面没有的规格
+        if (specsS[index] !== text && !bool) return;
+        // 根据text判断是否已经被选中了
+        specsS[index] = specsS[index] === text ? "" : text;
+        console.log("test====", text);
+        console.log("当前选中 ===", specsS)
+        setSpecsS(specsS.slice());
+    };
 
-  return (
-    <div className="container">
-      {specList.map(({ title, list }, index) => (
-        <div key={index}>
-          <p className="title">{title}</p>
-          <div className="specBox">
-            {list.map((value, i) => {
-              const isOption = optionSpecs.includes(value); // 当前规格是否可选
-              const isActive = specsS.includes(value); // 当前规格是否被选
-              return (
-                <span
-                  key={i}
-                  className={classNames({
-                    specOption: isOption,
-                    specAction: isActive,
-                    specDisabled: !isOption,
-                  })}
-                  onClick={() => handleClick(isOption, value, index)}
-                >
+    return (
+        <div className="container">
+            {/*{console.log("与当前选中项中可选择的项=====", optionSpecs)}*/}
+            {specList.map(({title, list}, index) => (
+                <div key={index}>
+                    <p className="title">{title}</p>
+                    <div className="specBox">
+                        {list.map((value, i) => {
+                            const isOption = optionSpecs.includes(value); // 当前规格是否可选
+                            const isActive = specsS.includes(value); // 当前规格是否被选
+                            // console.log("isOption====", value + "=====" + isOption);
+                            // console.log("isActive====", isActive);
+
+                            return (
+                                <span
+                                    key={i}
+                                    className={classNames({
+                                        specOption: isOption,
+                                        specAction: isActive,
+                                        specDisabled: !isOption,
+                                    })}
+                                    onClick={() => handleClick(isOption, value, index)}
+                                >
                   {value}
                 </span>
-              );
-            })}
-          </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Spec;
--- src/utils/adjoin-martix.ts
+++ src/utils/adjoin-martix.ts
@@ -1,89 +1,103 @@
 export type AdjoinType = Array<string>;
 
 export default class AdjoinMatrix {
-  vertex: AdjoinType; // 顶点数组
-  quantity: number; // 矩阵长度
-  adjoinArray: Array<number>; // 矩阵数组
+    vertex: AdjoinType; // 顶点数组(所有可选项)
+    quantity: number; // 矩阵长度
+    adjoinArray: Array<number>; // 矩阵数组
 
-  constructor(vertx: AdjoinType) {
-    this.vertex = vertx;
-    this.quantity = this.vertex.length;
-    this.adjoinArray = [];
-    this.init();
-  }
-  // 初始化数组
-  init() {
-    this.adjoinArray = Array(this.quantity * this.quantity).fill(0);
-  }
+    constructor(vertx: AdjoinType) {
+        console.log("AdjoinMatrix constructor====", vertx)
+        this.vertex = vertx;
+        this.quantity = this.vertex.length;
+        this.adjoinArray = [];
+        this.init();
+    }
 
-  /*
-   * @param id string
-   * @param sides Array<string>
-   *  传入一个顶点，和当前顶点可达的顶点数组，将对应位置置为1
-   */
-  setAdjoinVertexs(id: string, sides: AdjoinType) {
-    const pIndex = this.vertex.indexOf(id);
-    sides.forEach((item) => {
-      const index = this.vertex.indexOf(item);
-      this.adjoinArray[pIndex * this.quantity + index] = 1;
-    });
-  }
+    // 初始化数组  长度：顶点 * 顶点
+    init() {
+        this.adjoinArray = Array(this.quantity * this.quantity).fill(0);
+    }
 
-  /*
-   * @param id string
-   * 传入顶点的值，获取该顶点的列
-   */
-  getVertexCol(id: string) {
-    const index = this.vertex.indexOf(id);
-    const col: Array<number> = [];
-    this.vertex.forEach((item, pIndex) => {
-      col.push(this.adjoinArray[index + this.quantity * pIndex]);
-    });
-    return col;
-  }
+    /*
+     * @param id string  当前顶点
+     * @param sides Array<string> 可达数组
+     *  传入一个顶点，和当前顶点可达的顶点数组，将对应位置置为1
+     */
+    setAdjoinVertexs(id: string, sides: AdjoinType) {
+        //确定当前顶点所在的行
+        const pIndex = this.vertex.indexOf(id);
+        sides.forEach((item) => {
+            //顶点所在的列
+            const index = this.vertex.indexOf(item);
+            this.adjoinArray[pIndex * this.quantity + index] = 1;
+        });
+    }
 
-  /*
-   * @param params Array<string>
-   * 传入一个顶点数组，求出该数组所有顶点的列的合
-   */
-  getColSum(params: AdjoinType) {
-    const paramsVertex = params.map((id) => this.getVertexCol(id));
-    const paramsVertexSum: Array<number> = [];
-    this.vertex.forEach((item, index) => {
-      const rowtotal = paramsVertex
-        .map((value) => value[index])
-        .reduce((total, current) => {
-          total += current || 0;
-          return total;
-        }, 0);
-      paramsVertexSum.push(rowtotal);
-    });
-    return paramsVertexSum;
-  }
+    /*
+     * @param id string
+     * 传入顶点的值，获取该顶点的列
+     */
+    getVertexCol(id: string) {
+        const index = this.vertex.indexOf(id);
+        const col: Array<number> = [];
+        this.vertex.forEach((item, pIndex) => {
+            col.push(this.adjoinArray[index + this.quantity * pIndex]);
+        });
+        return col;
+    }
 
-  /*
-   *  @param params Array<string>
-   * 传入一个顶点数组，求出并集
-   */
-  getCollection(params: AdjoinType) {
-    const paramsColSum = this.getColSum(params);
-    let collections: AdjoinType = [];
-    paramsColSum.forEach((item, index) => {
-      if (item && this.vertex[index]) collections.push(this.vertex[index]);
-    });
-    return collections;
-  }
+    /*
+     * @param params Array<string>
+     * 传入一个顶点数组，根据顶点数组求出每个顶点的连线
+     */
+    getColSum(params: AdjoinType) {
+        //返回的是一个数组列表
+        const paramsVertex = params.map((id) => this.getVertexCol(id));
+        console.log("getColSum===", paramsVertex)
+        const paramsVertexSum: Array<number> = [];
+        this.vertex.forEach((item, index) => {
+            //求和
+            const rowtotal = paramsVertex
+                .map((value) => value[index])
+                .reduce((total, current) => {
+                    total += current || 0;
+                    return total;
+                }, 0);
+            paramsVertexSum.push(rowtotal);
+        });
+        return paramsVertexSum;
+    }
 
-  /*
-   *  @param params Array<string>
-   * 传入一个顶点数组，求出交集
-   */
-  getUnions(params: AdjoinType) {
-    const paramsColSum = this.getColSum(params);
-    let unions: AdjoinType = [];
-    paramsColSum.forEach((item, index) => {
-      if (item >= params.length && this.vertex[index]) unions.push(this.vertex[index]);
-    });
-    return unions;
-  }
+    /*
+     *  @param params Array<string>
+     * 传入一个顶点数组，求出并集
+     */
+    getCollection(params: AdjoinType) {
+        const paramsColSum = this.getColSum(params);
+        let collections: AdjoinType = [];
+        paramsColSum.forEach((item, index) => {
+            if (item && this.vertex[index]) collections.push(this.vertex[index]);
+        });
+        return collections;
+    }
+
+    /*
+     *  @param params Array<string>
+     * 传入一个顶点数组(当前选中的项)，求出交集
+     */
+    getUnions(params: AdjoinType) {
+        //返回一个数组，数组是当前点的连线数量
+        const paramsColSum = this.getColSum(params);
+        console.log("params====", params)
+        console.log("getUnions====", paramsColSum);
+        let unions: AdjoinType = [];
+        paramsColSum.forEach((count, index) => {
+            console.log("item===", count);
+            //为什么要大于params.length？这个方法是想获取符合选中项的item，所以只需要获取当前连线数量>=当前选中的点的数量就好
+            if (count >= params.length && this.vertex[index]) {
+                unions.push(this.vertex[index]);
+            }
+        });
+        return unions;
+    }
 }
